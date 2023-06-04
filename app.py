fa2b7351e9535bc3261b65f919bdf1b928502f5f
JV-X <xjv1195275315@gmail.com>
2023-06-04 18:39:34 +0800

    项目结构搭建

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from apps.cms import bp as cms_bp
+from apps.common import bp as common_bp
+from apps.front import bp as front_bp
+import config
 
 app = Flask(__name__)
+app.config.from_object(config)
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+app.register_blueprint(cms_bp)
+app.register_blueprint(common_bp)
+app.register_blueprint(front_bp)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8000)

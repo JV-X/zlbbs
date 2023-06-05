933d940cc1619fdd19152a54403730664feb6a18
JV-X <xjv1195275315@gmail.com>
2023-06-05 11:02:29 +0800

    登录页面csrf保护

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from apps.common import bp as common_bp
 from apps.front import bp as front_bp
 import config
 from exts import db, migrate
+from flask_wtf import CSRFProtect
 
 
 def create_app():
@@ -18,6 +19,8 @@ def create_app():
     app.register_blueprint(cms_bp)
     app.register_blueprint(common_bp)
     app.register_blueprint(front_bp)
+
+    CSRFProtect(app)
 
     app.cli.command('create_cms_user')(commands.create_cms_user)
     return app

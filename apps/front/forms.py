456dd61d693a160a18b84f242c3fbba9ed402350
JV-X <xjv1195275315@gmail.com>
2023-06-13 12:21:45 +0800

    发布帖子后台逻辑完成

--- apps/front/forms.py
+++ apps/front/forms.py
@@ -1,7 +1,8 @@
 from ..forms import BaseForm
-from wtforms import StringField
-from wtforms.validators import Regexp, EqualTo,ValidationError
+from wtforms import StringField, IntegerField
+from wtforms.validators import Regexp, EqualTo, ValidationError, InputRequired
 from utils import zlcache
+
 
 class SignupForm(BaseForm):
     telephone = StringField(validators=[Regexp(r'1[345789]\d{9}', message='手机号码格式不对')])
@@ -9,12 +10,12 @@ class SignupForm(BaseForm):
     username = StringField(validators=[Regexp(r'.{2,20}', message='用户名格式不对')])
     password1 = StringField(validators=[Regexp(r'[0-9a-zA-Z_\.]{6,20}', message='密码格式不对')])
     password2 = StringField(validators=[EqualTo('password1', message='两次密码不一样')])
-    graph_captcha = StringField(validators=[Regexp(r'\w{4}',message='图形验证码格式不对')])
+    graph_captcha = StringField(validators=[Regexp(r'\w{4}', message='图形验证码格式不对')])
 
     def validate_sms_captcha(self, field):
         sms_captcha = field.data
         telephone = self.telephone.data
-        print(telephone,sms_captcha,self.username.data,field)
+        print(telephone, sms_captcha, self.username.data, field)
         sms_captcha_mem = zlcache.get(telephone)
         if not sms_captcha_mem or sms_captcha_mem.lower() != sms_captcha.lower():
             raise ValidationError(message='验证码验证失败')
@@ -25,7 +26,14 @@ class SignupForm(BaseForm):
         if not graph_captcha_mem:
             raise ValidationError(message='图形验证码错误')
 
+
 class SigninForm(BaseForm):
     telephone = StringField(validators=[Regexp(r'1[345789]\d{9}', message='手机号码格式不对')])
     password = StringField(validators=[Regexp(r'[0-9a-zA-Z_\.]{6,20}', message='密码格式不对')])
     remember = StringField()
+
+
+class AddPostForm(BaseForm):
+    title = StringField(validators=[InputRequired(message='请传入标题')])
+    content = StringField(validators=[InputRequired(message='请传入内容')])
+    board_id = IntegerField(validators=[InputRequired(message='请传入版块ID')])
--- apps/front/views.py
+++ apps/front/views.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, views, render_template, url_for, make_response, request, session
 
 from utils import restful, safeutils
-from .forms import SignupForm, SigninForm
+from .forms import SignupForm, SigninForm, AddPostForm
 from .models import FrontUser
 from exts import db
 import config
-from ..models import BannerModel, BoardModel
+from .decorators import login_required
+from ..models import BannerModel, BoardModel, PostModel
 
 bp = Blueprint('front', __name__)
 
@@ -20,6 +21,29 @@ def index():
     }
     return render_template('front/front_index.html', **context)
 
+
+@bp.route('/apost', methods=['POST', 'GET'])
+@login_required
+def apost():
+    if request.method == 'GET':
+        return render_template('front/front_apost.html')
+    else:
+        form = AddPostForm(request.form)
+        if form.validate():
+            title = form.title.data
+            content = form.content.data
+            board_id = form.board_id.data
+            board = BoardModel.query.get(board_id)
+            if not board:
+                return restful.params_error('没有这个版块')
+            else:
+                post = PostModel(title=title, content=content)
+                post.board = board
+                db.session.add(post)
+                db.session.commit()
+                return restful.success()
+        else:
+            return restful.params_error(message=form.get_error())
 
 class SignupView(views.MethodView):
     def get(self):

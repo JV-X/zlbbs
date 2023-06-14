f404570105202b785f38251c3ce0bb50658f0286
JV-X <xjv1195275315@gmail.com>
2023-06-14 12:03:20 +0800

    评论布局和功能实现（2）

--- apps/front/views.py
+++ apps/front/views.py
@@ -49,7 +49,7 @@ def post_detail(post_id):
     return render_template('front/front_pdetail.html', post=post)
 
 
-@bp.route('/acomment')
+@bp.route('/acomment',methods=['POST'])
 @login_required
 def add_comment():
     form = AddCommentForm(request.form)
--- static/front/js/front_pdetail.js
+++ static/front/js/front_pdetail.js
@@ -1,6 +1,32 @@
 $(function () {
     var ue = UE.getEditor('editor',{
     'serverUrl':'/ueditor/upload/',
-
+    })
+    window.ue = ue
+})
+$(function () {
+    $('#comment-btn').click(function (event) {
+        event.preventDefault();
+        var loginTag = $('#login-tag').attr('data-is-login')
+        if(!loginTag){
+            window.location = '/signin/'
+        }else{
+            var content = window.ue.getContent()
+            var post_id =$('#post-content').attr('data-id')
+            zlajax.post({
+                'url':'/acomment',
+                'data': {
+                    'content':content,
+                    'post_id':post_id,
+                },
+                'success':function (data){
+                    if(data['code']==200) {
+                        window.location.reload()
+                    } else {
+                        zlalert.alertInfo(data['message'])
+                    }
+                }
+            })
+        }
     })
 })
